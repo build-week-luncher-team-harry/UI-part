b101cdc74d32c6e221e00592b5da11fadcaf6980
bmoir23 <brianmoir80@gmail.com>
2019-08-26 19:27:41 -0400

    added donations bar

--- js/index.js
+++ js/index.js
@@ -7,3 +7,17 @@ function changeText(button) {
   }
 
 //   counter
+function move() {
+    var elem = document.getElementById("bar"); 
+    var width = 100;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1 + '%';
+      }
+    }
+  }
